1b3320bd2ccc57b339cf7022d8bfdb912e73b566
Mendepoojitha123 <153059453+Mendepoojitha123@users.noreply.github.com>
2023-12-20 11:15:30 +0530

    Add files via upload
    COMMIT IN FRST

---
+++ src/test/java/com/trinadh/Class1.java
@@ -0,0 +1,14 @@
+package com.trinadh;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println("i am a developer i am executing build job and it is a intial setup");
+		System.out.println("i am a deployment team i will executed deployment after the build");
+		System.out.println(" hi i am  testing team i have completed my testing");
+		System.out.println("hi i am a release team i have released the application");
+				
+
+	}
+
+}
